10fdade1dd1e1e26f1698864b3c811299fb26ac8
mabur71 <mabur71@ya.ru>
2016-03-10 06:17:55 +0000

    failed 3_

---
+++ ask/qa/urls.py
@@ -0,0 +1,47 @@
+from django.conf.urls import patterns, include, url
+
+
+
+from django.contrib import admin
+
+admin.autodiscover()
+
+
+
+#from ask import qa
+
+import qa
+
+
+
+urlpatterns = patterns('',
+
+    # Examples:
+
+    # url(r'^$', 'ask.views.home', name='home'),
+
+    # url(r'^blog/', include('blog.urls')),
+
+
+
+    #url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', include('qa.urls')),
+
+    url(r'^login/', include('qa.urls')),
+
+    url(r'^signup/', include('qa.urls')),
+
+    #url(r'^question/(?P<question_id>[^/]+)/', include(qa.views.test)),
+
+    url(r'^question/', include('qa.urls')),
+
+    url(r'^ask/', include('qa.urls')),
+
+    url(r'^popular/', include('qa.urls')),
+
+    url(r'^new/', include('qa.urls')),
+
+    #url(r'^', include('qa.urls')),
+
+)
